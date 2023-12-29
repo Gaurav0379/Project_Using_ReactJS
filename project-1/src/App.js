921cbd41511f9f01a583716720910561bd1d1b5d
Gaurav0379 <154987975+Gaurav0379@users.noreply.github.com>
2023-12-29 15:23:01 +0000

    Done

---
+++ project-1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassComponent from './component/ClassComponent.jsx';
+import FunctionComponent from './component/FunctionComponent';
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello World!!!</h1>
+    <ClassComponent/>
+    <FunctionComponent/>
+      </div>
+  );
+}
+
+export default App;
---
+++ project-1/src/component/FunctionComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FunctionComponent(){
+    return(
+        <div className='Fuc'>
+            <h3>Fuction Component</h3>
+        </div>
+    );
+}
+
+export default FunctionComponent;
